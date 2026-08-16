bc397dee8234b5e181be9fa2051e16471a23f059
Tyson-Sujon <osiassujon@gmail.com>
2026-08-16 16:41:24 +0600

    fully not completed

---
+++ string.js
@@ -0,0 +1,28 @@
+/**
+ * string koi vabe likha jai
+ * (`  `) eta dea string declear kore backtic
+ * string onekta array er moto
+ * length method er kaj holo array te
+ * koita jaiga nise ta dekhe
+ */
+
+const country = "bangladesh";
+const division = "Newkhale";
+const district = `B-Baria`;
+const thana = new String("demra");
+
+console.log(typeof country);
+console.log(typeof division);
+console.log(typeof district);
+console.log(typeof thana);
+
+//array length use kore index er element count kora jai
+const number = [54, 98, 78, 21, 65];
+console.log(number.length);
+/**
+ * string similerly count korche
+ */
+console.log(district.length);
+//length borno dekhe na, kotutuku jaiga nise ta dekhe
+const capital = "Dha ka";
+console.log(capital.length);
